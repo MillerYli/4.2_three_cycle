eab730417764247cd52c733669f9c6e01bdb9f30
Yulia Miller <miller82.98@mail.ru>
2022-12-14 22:52:58 -0800

    Добавьте файлы проекта.

---
+++ 4.2. three cycle/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Mynamespase
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //первый цикл через for
+            Console.WriteLine("Цикл for");
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                Console.WriteLine(i);
+
+                switch (Console.ReadLine())
+                {
+
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("This is your super color!");
+                        break;
+                }
+            }
+            //второй цикл через while
+            Console.WriteLine("Цикл while");
+            int k = 0;
+
+            while(k < 3)
+            {
+                Console.WriteLine(k);
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("This is your super color!");
+                        break;
+                }
+
+                k++;
+            }
+
+            //третий цикл через do... while
+            Console.WriteLine("Цикл do... while");
+            int t = 0;
+            do
+            {
+                Console.WriteLine(t);
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("This is your super color!");
+                        break;
+                }
+                t++;
+            }
+            while (t < 3);
+        }
+    }
+}
